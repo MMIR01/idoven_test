c6e487cd3eba1a49d0ba1fc8b5aa0fe86edbd190
mmir01 <www.miralles@gmail.com>
2024-10-17 18:24:11 +0200

    Updated Jenkins persisten data folder

--- Exercise1-API/book-store/test_environment/postJenkinsInstallation.ps1
+++ Exercise1-API/book-store/test_environment/postJenkinsInstallation.ps1
@@ -2,8 +2,11 @@ Write-Host "Stopping docker container"
 docker stop testenv
 
 Write-Host "Copying jenkins jobs images"
-ECHO D | xcopy .\jenkins_config\config.xml .\jenkinsdata\config.xml
-ECHO D | xcopy .\jenkins_config\jobs\ .\jenkinsdata\jobs\ /s /e /h
+# Update this value with your own path
+$user_path = $env:USERPROFILE
+$jenkins_folder = $user_path + "\Desktop\Programas\"
+ECHO D | xcopy .\jenkins_config\config.xml $jenkins_folder\jenkinsdata\config.xml
+ECHO D | xcopy .\jenkins_config\jobs\ $jenkins_folder\jenkinsdata\jobs\ /s /e /h
 
 Write-Host "Start container again"
 docker start testenv
--- Exercise1-API/book-store/test_environment/testDocker.ps1
+++ Exercise1-API/book-store/test_environment/testDocker.ps1
@@ -9,24 +9,25 @@ ECHO y | docker image prune
 Write-Host "Copying needed folders"
 $currentFolder = (pwd).path
 cd..
-ECHO D | xcopy .\configuration\* .\test_environment\configuration/s /e /h
-ECHO D | xcopy .\datasets\* .\test_environment\datasets/s /e /h
-ECHO D | xcopy .\test_suite\* .\test_environment\test_suite /s /e /h
+ECHO D | xcopy .\test\configuration\* .\test_environment\configuration/s /e /h
+ECHO D | xcopy .\test\datasets\* .\test_environment\datasets/s /e /h
+ECHO D | xcopy .\test\test_suite\* .\test_environment\test_suite /s /e /h
 cd $currentFolder
 
 Write-Host "Updating docker image"
 docker build -t testenvimage . -f DockerfileTest
 
+# This is a persistent volume for Jenkins, so if we reset the container, the data is not lost
+# Update this value with your own path
+$user_path = $env:USERPROFILE
+$jenkins_folder = $user_path + "\Desktop\Programas\"
 Write-Host "Create volume"
-docker volume create --opt type=none --opt o=bind --opt device=$currentFolder/jenkinsdata jenkinsdata
+
+docker volume create --opt type=none --opt o=bind --opt device=$jenkins_folder/jenkinsdata jenkinsdata
 
 Write-Host "Run container"
-docker run -d --name testenv -p 8080:8080 -v jenkinsdata:/var/jenkins_home -v /var/run/docker.sock:/var/run/docker.sock -u root testenvimage
-# Run as root to avoid permission issues
-#docker run -itd -e JENKINS_USER=$(id -u) \
-#-v /var/run/docker.sock:/var/run/docker.sock \
-#-v $(pwd)/jenkins_home:/var/jenkins_home \
-#-v $(which docker):/usr/bin/docker \
+# Expose docker socket so container can communicate with docker in the host
+docker run -d --name testenv -p 8080:8080 -v $jenkins_folder/jenkinsdata:/var/jenkins_home -v /var/run/docker.sock:/var/run/docker.sock -u root testenvimage
 
 cd $currentFolder
 Remove-Item .\configuration\ -Force -Recurse
